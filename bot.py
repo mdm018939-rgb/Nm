e789bc39c480ccbeb9c3ed38944a950561b098d6
mdm018939-rgb <mdm018939@gmail.com>
2026-09-06 13:58:29 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -20,7 +20,7 @@ OTP_API_URL = "https://api.2oo9.cloud/MXS47FLFX0U/tness/@public/api/success-otp"
 # 🕒 Change Number কুলডাউন কনফিগারেশন
 user_last_change = {}  # {user_id: timestamp} — কুলডাউন ট্র্যাক করার জন্য
 waiting_users = set()
-CHANGE_COOLDOWN = 4  # সেকেন্ড
+CHANGE_COOLDOWN = 3  # সেকেন্ড
 
 # 🔒 ফোর্স জয়েন কনফিগারেশন
 CHANNEL_ID = -1002969454179          # চ্যানেলের numeric chat_id (বট এই চ্যানেলে admin থাকতে হবে)
